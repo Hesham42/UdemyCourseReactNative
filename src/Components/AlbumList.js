1dbe27d93fd175166d1f78f75657ac12c34c2238
Hesham <hmostafa3bd@gmail.com>
2019-05-07 18:04:42 +0200

    State part 1

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -7,12 +7,15 @@ import axios from 'axios';
 
 // const AlbumList = () => {
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {   
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => console.log(response));
+        .then(response => this.setState({ albums: response.data }));
     }
 
     render() {
+        console.log(this.state);
     return (
          <View>
             <Text>AlbumList</Text>
