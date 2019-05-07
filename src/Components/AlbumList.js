559f754a3b3aa379f11261261e80827ce062c8d9
Hesham <hmostafa3bd@gmail.com>
2019-05-07 17:36:22 +0200

    Load data By Oxios

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 
 import { Text, View } from 'react-native';
 
+import axios from 'axios';
+
 // const AlbumList = () => {
 class AlbumList extends Component {
     componentWillMount() {   
-        console.log('componentWillMount in AlbumList');
-        debugger;
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response));
     }
 
     render() {
